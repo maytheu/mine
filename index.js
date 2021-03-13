963708688a4712774fa35e59d4f5ee7858a3bbca
Maytheu <maytheu98@gmail.com>
2021-03-13 22:53:02 +0100

    path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require('path')
 
 require("dotenv").config();
 
@@ -28,7 +29,7 @@ require("./routes/resume")(app);
 // DEFAULT
 if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
   });
 }
 
