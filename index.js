2c8ea873504cac9845302f49df8ec102145d5474
Maytheu <maytheu98@gmail.com>
2021-03-13 22:38:10 +0100

    adding build file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({credentials: true,
-  origin: "http://localhost:3000"}));
+  origin: "https://maytheuresume.herokuapp.com/"}));
 
 require("./models/mainSchema.js");
 require("./models/userSchema.js");
@@ -20,8 +20,17 @@ mongoose.connect(process.env.DATABASE_URL, {
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static("client/build"));
+
 require("./routes/user")(app);
 require("./routes/resume")(app);
+
+// DEFAULT
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
