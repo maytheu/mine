7f4cc9826f03636b45e51bc0c6ba72de0accd4f2
Maytheu <maytheu98@gmail.com>
2021-03-24 21:48:15 +0100

    final commit

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors(
-  //{ credentials: true, origin: "http://localhost:3000" }
-  ));
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
